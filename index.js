ae7d871067986771be2a1447c48e9eb4d29bd13d
bassant abdelgawad <bassant.agawad@gmail.com>
2020-03-18 21:45:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+// route handler
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email"/>
+        <input name="password" placeholder="password"/>
+        <input name="passwordConfirmation"placeholder="password confirmation"/>
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('listening');
+});
