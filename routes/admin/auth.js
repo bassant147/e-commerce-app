da599a416fb6daaa84e128869d9b2f68d7a9cefb
bassant abdelgawad <bassant.agawad@gmail.com>
2020-03-25 20:03:19 +0200

    reusable Error Handling Middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -18,14 +18,10 @@ router.post('/signup',
   requirePassword,
   requirePasswordConfirmation
 ], 
+handleErrors(signupTemplate),
 async (req, res) => {
-  const errors = validationResult(req);
-  
-  if (!errors.isEmpty()) {
-    return res.send(signupTemplate({ req, errors }));
-  }
 
-  const { email, password, passwordConfirmation } = req.body; 
+  const { email, password } = req.body; 
   
   // Create a user in our user repo to repesent this person
   const user = await usersRepo.create({ email, password});
@@ -48,12 +44,9 @@ router.get('/signin', (req, res) => {
 router.post('/signin', [
   requireEmailExists,
   requireValidPasswordForUser
-  ], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
+  ], 
+  handleErrors(signinTemplate),
+  async (req, res) => {
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
